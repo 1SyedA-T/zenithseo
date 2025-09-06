c400554cebe92f1e4f1c96d53187ea48c1670894
1SyedA-T <faisal.houser47@gmail.com>
2025-09-06 22:12:26 +0500

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Welcome to ZenithSEO</h1>
+      <p>Your AI + SEO SaaS tool is under construction!</p>
+    </div>
+  );
+}
 
